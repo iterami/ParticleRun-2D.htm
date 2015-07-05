23e4d7b079a5c06b9d8054d36d92724c784825fc
Jan Horava <czechinthemail@gmail.com>
2015-07-05 16:48:26 -0700

    moved buffer display to css file

--- js/particlerun-2d.js
+++ js/particlerun-2d.js
@@ -242,7 +242,7 @@ function setmode(newmode, newgame){
 
         // If it's a newgame from the main menu, setup canvas and buffers.
         if(newgame){
-            document.getElementById('page').innerHTML = '<canvas id=canvas></canvas><canvas id=buffer style=display:none></canvas>';
+            document.getElementById('page').innerHTML = '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
 
             buffer = document.getElementById('buffer').getContext('2d');
             canvas = document.getElementById('canvas').getContext('2d');
