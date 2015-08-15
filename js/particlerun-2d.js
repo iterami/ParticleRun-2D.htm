2d9deb609b7b901ab6a009d2fe32913475042143
Jan Horava <czechinthemail@gmail.com>
2015-08-15 11:13:47 -0700

    added false alpha contextAttribute

--- js/particlerun-2d.js
+++ js/particlerun-2d.js
@@ -246,8 +246,12 @@ function setmode(newmode, newgame){
         if(newgame){
             document.getElementById('page').innerHTML = '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
 
-            buffer = document.getElementById('buffer').getContext('2d');
-            canvas = document.getElementById('canvas').getContext('2d');
+            buffer = document.getElementById('buffer').getContext('2d', {
+              'alpha': false,
+            });
+            canvas = document.getElementById('canvas').getContext('2d', {
+              'alpha': false,
+            });
 
             resize();
         }
