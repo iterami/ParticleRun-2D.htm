20016a7922eff9a51f59630a77625616d77f98d3
Jan Hořava <czechinthemail@gmail.com>
2018-08-31 20:04:30 -0700

    updated core_ui_update() usage

--- js/main.js
+++ js/main.js
@@ -195,7 +195,7 @@ function repo_init(){
       },
       'storage-menu': '<table><tr><td><input id=max-particles><td>Max Particles<tr><td><input id=particle-height><td>Particle Height<tr><td><input id=particle-width><td>Particle Width<tr><td><input id=scroll-speed><td>Scroll Speed</table>',
       'title': 'ParticleRun-2D.htm',
-      'ui': '<span id=ui-particles></span> Particles',
+      'ui': '<span id=particles></span> Particles',
     });
     canvas_init();
 }
