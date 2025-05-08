39a19ba4d29467e9585781c3f85819f4df7967d2
Jan Hořava <czechinthemail@gmail.com>
2025-05-08 10:58:08 -0700

    updated mouse events to pointer events

--- repo.js
+++ repo.js
@@ -212,9 +212,9 @@ function repo_drawlogic(){
 }
 
 function repo_logic(){
-    if(core_mouse['down-0']){
-        camera_x -= core_mouse['movement-x'];
-        camera_y -= core_mouse['movement-y'];
+    if(core_pointer['down-0']){
+        camera_x -= core_pointer['movement-x'];
+        camera_y -= core_pointer['movement-y'];
     }
     if(core_keys[core_storage_data['move-↓']]['state']){
         camera_y += core_storage_data['scroll-speed'];
@@ -337,7 +337,7 @@ function repo_init(){
       },
       'info': '<button id=test type=button>Test Level</button>',
       'menu': true,
-      'mousebinds': {},
+      'pointerbinds': {},
       'storage': {
         'particle-height': 5,
         'particle-max': 1000,
